f61d0ba477ebab1e62612e3964b4241cb32df128
Ezequiel Lowi <skulas@gmail.com>
2023-03-14 19:49:25 +0200

    begin auth impmlementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './users/entities/user.entity';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
+@ApiTags('auth')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  @ApiOkResponse({ type: UserEntity })
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodoItemsModule } from './todo-items/todo-items.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, TodoItemsModule],
+  imports: [AuthModule, PrismaModule, TodoItemsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
