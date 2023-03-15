7a23e608ac18598202bd9cb3826e0e9a1f9c97dd
Ezequiel Lowi <skulas@gmail.com>
2023-03-15 13:46:31 +0200

    move backend to backend folder first and add create user api

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Get, Request, Post, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Request,
+  Post,
+  UseGuards,
+  Req,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { UserEntity } from './users/entities/user.entity';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt.auth.guard';
+import { UsersService } from './users/users.service';
+import { CreateUserDto } from './users/dto/create-user.dto';
 
 @Controller()
 @ApiTags('auth')
@@ -12,6 +22,7 @@ export class AppController {
   constructor(
     private readonly authService: AuthService,
     private readonly appService: AppService,
+    private readonly userService: UsersService,
   ) {}
 
   @Get()
@@ -30,5 +41,18 @@ export class AppController {
   @ApiOkResponse({ type: UserEntity })
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  // @UseGuards(LocalAuthGuard)
+  @Post('register')
+  @ApiOkResponse({ type: UserEntity })
+  async register(@Body() createTodoItemDto: CreateUserDto) {
+    // There's a smart way to catch exceptions by filters in NestJS. Just a bit of an overkill for now.
+    try {
+      const newUser = await this.userService.create(createTodoItemDto);
+      return newUser;
+    } catch (e) {
+      return { error: 'Your email is already in use' };
+    }
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodoItemsModule } from './todo-items/todo-items.module';
 import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [AuthModule, PrismaModule, TodoItemsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
